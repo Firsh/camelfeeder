cac198d512b83693960fc53532ac4ae3de7ab01d
Firsh <firsh@justifiedgrid.com>
2024-07-09 16:22:14 +0200

    mar nincs mit tanitani neki illetve random legyen az 1szam tipp

--- teve.sh
+++ teve.sh
@@ -13,6 +13,9 @@ else
     set +a
 fi
 
+# Generate random integer between $tippmin and $tippmax
+tipp=$((RANDOM % (tippmax - tippmin + 1) + tippmin))
+
 ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.147 Safari/537.36"
 
 echo "Megprobalunk belepni..."
@@ -35,24 +38,24 @@ else
     exit
 fi
 
-echo "Tanitunk..."
-
-if curl --location --request POST "https://teveclub.hu/tanit.pet" \
-    --header "$ua" \
-    --header "Content-Type: application/x-www-form-urlencoded" \
-    --cookie-jar "cookie.txt" \
-    --cookie "cookie.txt" \
-    --data-urlencode "farmdoit=tanit" \
-    --data-urlencode "learn=Tanulj teve!" \
-    --silent \
-    --output - \
-    | grep -q "csak holnap tud tanulni"
-then
-    echo "Sikerult tanitani!"
-else
-    echo "Nem sikerult tanitani!"
-    exit
-fi
+# echo "Tanitunk..."
+# 
+# if curl --location --request POST "https://teveclub.hu/tanit.pet" \
+#     --header "$ua" \
+#     --header "Content-Type: application/x-www-form-urlencoded" \
+#     --cookie-jar "cookie.txt" \
+#     --cookie "cookie.txt" \
+#     --data-urlencode "farmdoit=tanit" \
+#     --data-urlencode "learn=Tanulj teve!" \
+#     --silent \
+#     --output - \
+#     | grep -q "csak holnap tud tanulni"
+# then
+#     echo "Sikerult tanitani!"
+# else
+#     echo "Nem sikerult tanitani!"
+#     exit
+# fi
 
 echo "Egyszamozunk..."
 
